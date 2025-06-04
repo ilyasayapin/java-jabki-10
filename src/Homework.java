77995c573191a079f98b7d54e3e57042bafbc4b3
ilyasayapin <kite@nxt.ru>
2025-06-04 09:13:26 +0300

    Homework 10.

--- src/Homework.java
+++ src/Homework.java
@@ -3,7 +3,7 @@ import java.util.Set;
 
 public class Homework {
 
-//    1.1 Создание множества. Создайте HashSet<String>, добавьте 5 слов и выведите все элементы.
+    // 1.1 Создание множества. Создайте HashSet<String>, добавьте 5 слов и выведите все элементы.
     public static Set<String> newSetString() {
         Set<String> set = new HashSet<>();
         set.add("Один");
@@ -11,10 +11,11 @@ public class Homework {
         set.add("Три");
         set.add("java");
         set.add("Пять");
+        set.add("Пять");
         return set;
     }
 
-//    1.2 Проверка на наличие элемента. Проверьте, содержится ли слово "java" в множестве.
+    // 1.2 Проверка на наличие элемента. Проверьте, содержится ли слово "java" в множестве.
     public static boolean chekWordJava(Set<String> set) {
         boolean chek = false;
         for (String str : set) {
@@ -25,11 +26,15 @@ public class Homework {
         return chek;
     }
 
-//    1.3 Удаление элемента. Удалите слово "python" из множества (если оно есть).
+    // 1.3 Удаление элемента. Удалите слово "python" из множества (если оно есть).
     public static boolean RemoveFromSet(Set<String> set) {
         boolean isRemoved = set.remove("python");
         return isRemoved;
     }
 
+    // 1.4 Подсчёт уникальных. Дан список строк с повторами. Добавьте их в HashSet и выведите количество уникальных слов.
+    public static int countUnique(Set<String> set) {
+        return set.size();
+    }
 
 }
