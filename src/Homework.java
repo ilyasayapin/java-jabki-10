352552df1b45c74c50f208a487da7f475c013975
ilyasayapin <kite@nxt.ru>
2025-06-04 08:50:11 +0300

    Homework 10.

--- src/Homework.java
+++ src/Homework.java
@@ -1,4 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Homework {
+
+//    1.1 Создание множества. Создайте HashSet<String>, добавьте 5 слов и выведите все элементы.
+    public static Set<String> newSetString() {
+        Set<String> set = new HashSet<>();
+        set.add("Один");
+        set.add("Два");
+        set.add("Три");
+        set.add("Четыре");
+        set.add("Пять");
+        return set;
+    }
+
+//    1.2 Проверка на наличие элемента. Проверьте, содержится ли слово "java" в множестве.
+    public static boolean chekWordJava(Set<String> set) {
+
+
+    }
 
 
 }
