53d32cc12740795c4fbab5e1aad8f942c20e6d82
ilyasayapin <kite@nxt.ru>
2025-06-06 17:09:59 +0300

    Homework 10.

--- src/Homework.java
+++ src/Homework.java
@@ -21,19 +21,12 @@ public class Homework {
 
     // 1.2 Проверка на наличие элемента. Проверьте, содержится ли слово "java" в множестве.
     public static boolean cheсkWordJava(Set<String> set) {
-        boolean chek = false;
-        for (String str : set) {
-            if (str.equals("java")) {
-                chek = true;
-            }
-        }
-        return chek;
+        return set.contains("java");
     }
 
     // 1.3 Удаление элемента. Удалите слово "python" из множества (если оно есть).
     public static boolean removeFromSet(Set<String> set) {
-        boolean isRemoved = set.remove("python");
-        return isRemoved;
+        return set.remove("python");
     }
 
     // 1.4 Подсчёт уникальных. Дан список строк с повторами. Добавьте их в HashSet и выведите количество уникальных слов.
